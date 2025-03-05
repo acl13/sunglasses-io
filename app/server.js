0591751e7a6d38e0bc32cba58fb082505788223e
acl13 <128934431+acl13@users.noreply.github.com>
2025-03-05 11:22:03 -0500

    write and test endpoints GET /brands and GET /products

--- app/server.js
+++ app/server.js
@@ -1,31 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml'); // Replace './swagger.yaml' with the path to your Swagger file
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml"); // Replace './swagger.yaml' with the path to your Swagger file
 const app = express();
 
 app.use(bodyParser.json());
 
 // Importing the data from JSON files
-const users = require('../initial-data/users.json');
-const brands = require('../initial-data/brands.json');
-const products = require('../initial-data/products.json');
+const users = require("../initial-data/users.json");
+const brands = require("../initial-data/brands.json");
+const products = require("../initial-data/products.json");
 
 // Error handling
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("/brands", (request, response) => {
+  response.send(brands);
+  return response.end();
+});
+
+app.get("/products", (request, response) => {
+  response.send(products);
+  return response.end();
+});
 
 // Starting the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
--- test/server.test.js
+++ test/server.test.js
@@ -1,14 +1,97 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../app/server'); // Adjust the path as needed
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../app/server"); // Adjust the path as needed
 
 const should = chai.should();
 chai.use(chaiHttp);
 
 // TODO: Write tests for the server
 
-describe('Brands', () => {});
+describe("Brands", () => {
+  describe("/GET brands", () => {
+    it("should GET all the brands", (done) => {
+      chai
+        .request(server)
+        .get("/brands")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.length.should.be.eql(5);
+          done();
+        });
+    });
+  });
 
-describe('Login', () => {});
+  // describe('/GET brands/:id/products', () => {
+  //   it('should GET all the products for a specific brand', (done) => {
+  //     // Write test logic here
+  //     (done);
+  //   })
+  // })
+});
 
-describe('Cart', () => {});
+describe("Products", () => {
+  describe("/GET products", () => {
+    it("should GET all the products", (done) => {
+      chai
+        .request(server)
+        .get("/products")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.length.should.be.eql(11);
+          done();
+        });
+    });
+  });
+});
+
+describe("Login", () => {
+  // describe("/POST login", () => {
+  //   it("logs in an authenticated user", (done) => {
+  //     //Write test logic here
+  //     done();
+  //   });
+  // });
+});
+
+describe("Cart", () => {
+  // describe('/GET me/cart', () => {
+  //   it('gets the cart for an authenticated user', () => {
+  //     //arrange: login user/get user cart
+  //     // write test logic here
+  //     done()
+  //   })
+  // })
+  //
+  //
+  //
+  // describe("/POST me/cart", () => {
+  //   it("adds an item to user's cart", (done) => {
+  //     //arrange: login user, need product id in request body?
+  //     //write test logic here
+  //     done();
+  //   });
+  // });
+  //
+  //
+  //
+  // describe("/POST me/cart/:productId", () => {
+  //   it("Updates the quantity of a particular item in the user's cart", (done) => {
+  //     // arrange: login user
+  //     // will need to check cart quantity?
+  //     // write test logic here
+  //     done();
+  //   });
+  // });
+  //
+  //
+  //
+  // describe("/DELETE me/cart/:productId", () => {
+  //   it("deletes an item from the cart", (done) => {
+  //     // arrange: login user
+  //     // write test logic here
+  //     done();
+  //   });
+  // });
+});
